a6caf45916eafc01eb8c89af2109754e41a27812
Kang Min Wang <aminzai@aminzai.net>
2009-10-07 22:52:33 +0800

    modify Function

--- WaitPostDataParser.py
+++ WaitPostDataParser.py
@@ -28,13 +28,13 @@ for i in range( len( RawData ) ):
   data[ i ] =  RawData[ i ]
 
 while 1:
-        for i in data.keys():
-          print i , data[ i ]
-        DelNum = raw_input('Type num to del item, or type q to quit:')
-        if DelNum == 'q':
-          print 'Bye!'
-          sys.exit()
-        else:
-          data[ DelNum ] = ''
+ for i in data.keys():
+   print i , data[ i ]
+ DelNum = raw_input('Type num to del item, or type q to quit:')
+ if DelNum == 'q':
+   print 'Bye!'
+   sys.exit()
+ else:
+   data[ DelNum ] = ''
 
 
